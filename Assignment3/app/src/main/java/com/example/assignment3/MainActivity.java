a414f6a1c528c5456e4aba12649545f0293f5bb7
Muhammad Farooq <farooqawaisi6310@gmail.com>
2021-11-06 19:00:07 +0500

    repo button added with intent

--- Assignment3/app/src/main/java/com/example/assignment3/MainActivity.java
+++ Assignment3/app/src/main/java/com/example/assignment3/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.assignment3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button repoButton = findViewById(R.id.repo);
+        repoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadRepository();
+            }
+        });
+
+    }
+
+    public void loadRepository(){
+        Uri webUrl = Uri.parse("https://github.com/muhammad-farooq-786/intro-app.git");
+        Intent repoIntent = new Intent(Intent.ACTION_VIEW,webUrl);
+        startActivity(repoIntent);
     }
 }
