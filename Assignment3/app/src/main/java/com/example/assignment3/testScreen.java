831ee0f9a4013230e0eb32a764a374064256650a
Muhammad Farooq <farooqawaisi6310@gmail.com>
2021-11-11 19:45:59 +0500

    test screen complete

--- Assignment3/app/src/main/java/com/example/assignment3/testScreen.java
+++ Assignment3/app/src/main/java/com/example/assignment3/testScreen.java
@@ -14,12 +14,14 @@ import org.w3c.dom.Text;
 
 public class testScreen extends AppCompatActivity {
 
+    int count = 1;
+    int result = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_screen);
 
-        int count = 1;
+
         String[] questions =
                 new String[] {"Given word \"م\" is from which group of emission point:\na) ghunna\n b) halqiyah",
                         "Given word \"ع\" is from which group of emission point:\na) halqiyah\n b) ghunna",
@@ -28,24 +30,31 @@ public class testScreen extends AppCompatActivity {
                         "Given word \"ن\" is from which group of emission point:\na) tarfiyah\n b) halqiyah"};
         TextView qView = findViewById(R.id.QuestionView);
         TextView qNumber = findViewById(R.id.qNum);
+        qView.setText(questions[count-1]);
         Button aButton = findViewById(R.id.answerButton);
         aButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 EditText editText= findViewById(R.id.ansEdit);
-                if(editText.getText().toString().equals("a)")){
+                if(editText.getText().toString().equals("a")){
                     Toast.makeText(testScreen.this,"Your answer is correct",Toast.LENGTH_LONG).show();
+                    result++;
+                    count++;
                 }
-                else if (!editText.getText().toString().equals("a)") && !editText.getText().toString().equals("b)")){
+                else if (!editText.getText().toString().equals("a") && !editText.getText().toString().equals("b")){
                     Toast.makeText(testScreen.this,"Please write appropriate option",Toast.LENGTH_LONG).show();
                 }else {
                     Toast.makeText(testScreen.this,"Your answer is wrong",Toast.LENGTH_LONG).show();
+                    count++;
                 }
-                int count=1;
+                if(count==5){
+                    Intent intent = new Intent(testScreen.this,resultScreen.class);
+                    intent.putExtra("result",Integer.toString(result));
+                    startActivity(intent);
+                }
                 String qNum = Integer.toString(count);
-                qNumber.setText(qNum);
                 qView.setText(questions[count-1]);
-                count++;
+                qNumber.setText(qNum);
             }
         });
     }
