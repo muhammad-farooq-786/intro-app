3732cb08c35a521b6d8a4878499ff4c5e2416027
Muhammad Farooq <farooqawaisi6310@gmail.com>
2021-11-11 21:04:27 +0500

    result and share screen added

--- Assignment3/app/src/main/java/com/example/assignment3/resultScreen.java
+++ Assignment3/app/src/main/java/com/example/assignment3/resultScreen.java
@@ -2,7 +2,11 @@ package com.example.assignment3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class resultScreen extends AppCompatActivity {
 
@@ -10,5 +14,37 @@ public class resultScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_screen);
+        TextView result,score;
+        result = findViewById(R.id.resultView);
+        score = findViewById(R.id.scoreView);
+        Intent intent = getIntent();
+        String totalScore = intent.getStringExtra("result");
+        int resultScore = Integer.parseInt(totalScore);
+        if(resultScore > 2){
+            result.setText("Congrats! You have a good score");
+            score.setText(Integer.toString(resultScore));
+        }else {
+            result.setText("You have passed the test");
+            score.setText(Integer.toString(resultScore));
+        }
+        Button homeButton = findViewById(R.id.homeB);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(resultScreen.this,Makharij.class);
+                startActivity(intent1);
+            }
+        });
+
+        Button shareButton = findViewById(R.id.share);
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent();
+                intent2.setType("text/plain");
+                intent2.setAction(Intent.ACTION_SEND);
+                startActivity(intent2);
+            }
+        });
     }
 }
