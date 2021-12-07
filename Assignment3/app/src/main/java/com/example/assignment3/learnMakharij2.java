753d00f75411a47dcb4b08100e213b4ff86b7c25
Eskool18 <eskooleims@gmail.com>
2021-12-07 21:35:58 +0500

    app bar with up bar added in all activities

--- Assignment3/app/src/main/java/com/example/assignment3/learnMakharij2.java
+++ Assignment3/app/src/main/java/com/example/assignment3/learnMakharij2.java
@@ -1,11 +1,16 @@
 package com.example.assignment3;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class learnMakharij2 extends AppCompatActivity {
 
@@ -13,6 +18,11 @@ public class learnMakharij2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn_makharij2);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
 
 
         Button backButton = findViewById(R.id.back3);
@@ -41,5 +51,26 @@ public class learnMakharij2 extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.makharij:
+                Intent intent = new Intent(this, Makharij.class);
+                startActivity(intent);
+            case R.id.action_favorite:
+                Toast.makeText(getApplicationContext(),"Fav", Toast.LENGTH_LONG).show();
+            case R.id.test:
+                Toast.makeText(getApplicationContext(),"Test was clicked", Toast.LENGTH_SHORT).show();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
